61fc29fdbd56b87a46fd17e18c8dd97444bd7b17
Daniel Janus <dj@danieljanus.pl>
2014-09-28 16:47:44 +0200

    Logging in, adding cards.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,12 @@ class CardsController < ApplicationController
   def new
     @card = Card.new
   end
+
+  def create
+    user = User.find session[:user_id]
+    card_data = params.require(:card).permit :number
+    @card = Card.new user_id: session[:user_id], number: card_data[:number]
+    @card.save
+    redirect_to user
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by email: params["sessions"]["email"]
+    if user && user.authenticate(params["sessions"]["password"])
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
